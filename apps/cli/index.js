262686da2ba2ef64229c8cceff51b1fe11b9226f
madoyakimberley <kimmadoya06@gmail.com>
2026-06-23 03:57:02 +0300

    API Fix

--- apps/cli/index.js
+++ apps/cli/index.js
@@ -403,7 +403,7 @@ function getBaseApiUrl() {
   const rawAppUrl =
     process.env.NEXT_PUBLIC_APP_URL ||
     process.env.APP_URL ||
-    "http://localhost:3000";
+    "https://studioflow-dashboard.onrender.com";
 
   let cleanAppUrl = rawAppUrl.replace(/\/$/, "");
 
